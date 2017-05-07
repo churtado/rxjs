21637f41920a2674727660b29ee88b9e25b267a9
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-05-07 19:14:19 +0100

    first commit

---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import * as Rx from 'rxjs-es/Rx';
+
+console.clear();
+
+var source = ['1', '1', 'foo', '2', '3', '5', 'bar', '8', '13'];
+
+var result = source;
+
+console.log(result);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    'app': './app/main.ts'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loaders: [
+          {
+            loader: 'awesome-typescript-loader',
+          } 
+        ]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+};
