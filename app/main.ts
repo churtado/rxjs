8936537a1ffd0d2d6afbe8cd15b3defdb6e8d553
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-05-07 23:10:55 +0100

    latest changes

--- app/main.ts
+++ app/main.ts
@@ -1,12 +1,57 @@
 import * as Rx from 'rxjs-es/Rx';
+import { Observable } from 'rxjs-es/Observable';
 import * as $ from 'jquery';
 
-var requestStream = Rx.Observable.of('http://api.github.com/users');
+interface GitHubUser {
+    avatar_url:string;
+    events_url:string;
+    followers_url:string;
+    following_url: string;
+    gists_url: string;
+    gravatar_id: string;
+    html_url: string;
+    id: number;
+    login: string;
+    organizations_url: string;
+    received_events_url: string;
+    repos_url: string;
+    site_admin: boolean;
+    starred_url: string;
+    subscriptions_url: string;
+    type: string;
+    url: string;
+}
 
-requestStream.subscribe(requestUrl => {
-    $.getJSON(requestUrl)
-        .done(response => {
-            console.log(response);
-        });
-    
-});
+var requestStream: Observable<string> = Rx.Observable.of('http://api.github.com/users');
+
+var responseStream: Observable<any> = requestStream
+    .flatMap(requestUrl =>
+        Rx.Observable.fromPromise(Promise.resolve($.getJSON(requestUrl)))
+    );
+
+responseStream.subscribe(response => {
+    console.log(response);
+})
+
+function createSuggestionStream(responseStream: Observable<GitHubUser[]>): Observable<any>{
+    return responseStream.map(listUser => {
+        listUser[Math.floor(Math.random()*listUser.length)];    
+    })
+}
+
+var suggestion1Stream = createSuggestionStream(responseStream);
+var suggestion2Stream = createSuggestionStream(responseStream);
+var suggestion3Stream = createSuggestionStream(responseStream);
+
+function renderSuggestion(userData: GitHubUser, selector:string): void {
+    var element  = document.querySelector(selector);
+    var usernameEl = element.querySelector('.username');
+    usernameEl.setAttribute('href', userData.html_url);
+    usernameEl.textContent = userData.login;
+    var imgEl = element.querySelector('img');
+    imgEl.src = userData.avatar_url;
+}
+
+suggestion1Stream.subscribe(user => {
+    renderSuggestion(user, '.suggestion1');
+})
